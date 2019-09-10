4726e3f6bc5e101a308e0b1c0379d60cf0e60d29
unknown <34763906+iitsuraj@users.noreply.github.com>
2019-09-11 01:49:42 +0530

    first commit 2

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports ={
+    database: "mongodb://suraj:suraj@fmc-shard-00-00-fsipp.mongodb.net:27017,fmc-shard-00-01-fsipp.mongodb.net:27017,fmc-shard-00-02-fsipp.mongodb.net:27017/fmc?ssl=true&replicaSet=fmc-shard-0&authSource=admin&retryWrites=true&w=majority",
+    port: 3000,
+    secretKey: 'fmctrue',
+    publicKey:'BMiBp3lvCa9hJurodtvtZqavPnBMg1j33o4rxV_pm8J5c6NGVAVoLpHahJoqi88zyqmTZsGsZdqrqlPe8Enehog',
+    privateKey: 'UG9tl4mE8d57PsaGk4bEKkUZU4Xp09m-H6rp1JZKCFI'
+}
